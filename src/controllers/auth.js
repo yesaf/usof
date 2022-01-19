3ba000606f022a99b42f07f1e4b1cc74380466da
ruvim <ruvimsypa@gmail.com>
2022-01-19 19:28:44 +0200

    add login controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,7 +1,42 @@
-module.exports.login = (request, response) => {
-  response.json({
-    login: "This is login router"
-  });
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const keys = require("../config/keys");
+
+module.exports.login = async (request, response) => {
+  const { email, password } = request.body;
+
+  const [user] = await User.findUserByEmail(email);
+
+  if (!user) {
+    return response
+      .status(404)
+      .json({
+        type: "error",
+        msg: "No such user exists"
+      });
+  }
+
+  const passwordResult = bcrypt.compareSync(password, user.password);
+  if (!passwordResult) {
+    return response
+      .status(401)
+      .json({
+        type: "error",
+        msg: "Password mismatch"
+      });
+  }
+
+  const token = jwt.sign({
+    email: user.email,
+    userId: user.id,
+    iss: 'http://localhost:4000'
+  }, keys.jwt, { expiresIn: 3600 });
+
+  return response
+    .json({
+      token: `Bearer ${token}`
+    });
 };
 
 module.exports.register = (request, response) => {

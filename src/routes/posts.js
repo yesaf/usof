9d8aec0e35f0d6d59f11bffb447184422e657f31
ruvim <ruvimsypa@gmail.com>
2022-02-03 13:08:05 +0100

    add check authenticate for /api/posts

--- src/routes/posts.js
+++ src/routes/posts.js
@@ -1,21 +1,22 @@
 const { Router } = require('express');
 const controller = require('../controllers/posts');
+const passport = require("passport");
 const router = new Router();
 
-router.get('/', controller.getAll);
-router.post('/', controller.create);
+router.get('/', passport.authenticate('jwt', { session: false }), controller.getAll);
+router.post('/', passport.authenticate('jwt', { session: false }), controller.create);
 
-router.get('/:postId', controller.getPost);
-router.patch('/:postId', controller.updatePost);
-router.delete('/:postId', controller.deletePost);
+router.get('/:postId', passport.authenticate('jwt', { session: false }), controller.getPost);
+router.patch('/:postId', passport.authenticate('jwt', { session: false }), controller.updatePost);
+router.delete('/:postId', passport.authenticate('jwt', { session: false }), controller.deletePost);
 
-router.get('/:postId/comments', controller.getAllComments);
-router.post('/:postId/comments', controller.createComment);
+router.get('/:postId/comments', passport.authenticate('jwt', { session: false }), controller.getAllComments);
+router.post('/:postId/comments', passport.authenticate('jwt', { session: false }), controller.createComment);
 
-router.get('/:postId/categories', controller.getAllCategories);
+router.get('/:postId/categories', passport.authenticate('jwt', { session: false }), controller.getAllCategories);
 
-router.get('/:postId/like', controller.getAllLikes);
-router.post('/:postId/like', controller.like);
-router.delete('/:postId/like', controller.unlike);
+router.get('/:postId/like', passport.authenticate('jwt', { session: false }), controller.getAllLikes);
+router.post('/:postId/like', passport.authenticate('jwt', { session: false }), controller.like);
+router.delete('/:postId/like', passport.authenticate('jwt', { session: false }), controller.unlike);
 
 module.exports = router;
